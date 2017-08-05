adb4b512cb712c9b8f5e6eca272f238d64c87b77
Alexandre Muzio <ax.muzio@gmail.com>
2017-08-05 20:47:15 -0300

    Adding populate db script

--- ambev/src/main.js
+++ ambev/src/main.js
@@ -39,13 +39,3 @@ new Vue({
   },
   render (h) { return h(this.ViewComponent) }
 })
-
-/*// Examples
-setTimeout(function() {
-  // bar_events.save("id", "name", "date");
-  bar.save("Bar do Ze", "Rua dos Bobos, num 0");
-  bar.save("Bar do Peh", "Rua dos Bobs, num 1000");
-  bar.save("Bar do Calango", "Rua dos Otarios, num -1")
-  // console.log(bar.getAll());
-  // console.log(bar.getByQuery("address"))
-}, 4000);*/

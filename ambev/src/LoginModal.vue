b3ae24bff75bfe7b543ed6747a9118c0cff73cbd
Alexandre Muzio <ax.muzio@gmail.com>
2017-08-06 09:52:22 -0300

    Correcting search and login

--- ambev/src/LoginModal.vue
+++ ambev/src/LoginModal.vue
@@ -4,12 +4,20 @@
       <div @click="skip" class="modal-background"></div>
       <div class="modal-content">
         <div class="box">
+          <table class="table is-fullwidth">
+            <tbody>
+            <tr><td>
             <div class="block is-centered">
-              <a @click="login" class="button is-info is-large">Entrar com Facebook</a>
+              <a @click="login" class="button is-info is-large" style="width:100%">Entrar com Facebook</a>
             </div>
+            </td></tr>
+            <tr><td>
             <div class="block is-centered">
-              <a @click="skip" class="button is-light is-large">Pular</a>
+              <a @click="skip" class="button is-light is-large"  style="width:100%">Pular</a>
             </div>
+            </td></tr>
+            </tbody>
+            </table>
           </div>
       </div>
       <button class="modal-close is-large"></button>
@@ -18,6 +26,9 @@
 </template>
 
 <script>
+import * as firebase from 'firebase';
+
+
 export default {
   name: 'login-modal',
   data () {
@@ -37,7 +48,26 @@ export default {
       this.done = true
     },
     login: function() {
+      var provider = new firebase.auth.FacebookAuthProvider();
+
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
       console.log("login");
+      
       this.done = true
     }
   }
